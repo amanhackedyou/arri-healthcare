addfd38a3a174b675d0002ad48a2c9f8802e269d
Blanco237 <asongrandy9@gmail.com>
2024-12-15 19:12:05 +0100

    feat: Completed Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+import Images from "@/lib/assets";
+import Link from "next/link";
+import Hamburger from "hamburger-react";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className={styles.body}>
+      <Image
+        src={Images.logo}
+        width={180}
+        height={50}
+        className={styles.logo}
+        alt="Arri Healthcare"
+      />
+
+      <nav
+        className={`${styles.nav} ${menuOpen ? styles.open : styles.closed}`}
+      >
+        <div className={styles.bar}>
+          <Image
+            src={Images.logoIcon}
+            width={40}
+            height={40}
+            className={styles.icon}
+            alt="Arri Healthcare"
+          />
+          <Hamburger
+            toggled={menuOpen}
+            toggle={setMenuOpen}
+            size={38}
+            rounded
+          />
+        </div>
+        <ul>
+          <li>
+            <Link href="#">About Us</Link>
+          </li>
+          <li>
+            <Link href="#">Services</Link>
+          </li>
+          <li>
+            <Link href="#">Training</Link>
+          </li>
+          <li>
+            <Link href="#">Contact</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Career</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Hamburger toggled={menuOpen} toggle={setMenuOpen} size={38} rounded />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/lib/assets.ts
@@ -0,0 +1,7 @@
+const Images = {
+    logo: '/assets/images/logo.svg',
+    logoIcon: '/assets/images/logo_icon.svg',
+
+}
+
+export default Images;
