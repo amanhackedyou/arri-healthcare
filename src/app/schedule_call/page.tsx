803afcc803903435ec3834534152af67f03e78fa
Aman Programmer <ay12121230@gmail.com>
2024-12-26 05:59:50 +0530

    Updating the landing page

--- src/app/schedule_call/page.tsx
+++ src/app/schedule_call/page.tsx
@@ -20,6 +20,7 @@ const ScheduleACall = () => {
     const [preferredDate, setPreferredDate] = useState<string>(todayDate);
     const [preferredTime, setPreferredTime] = useState<string>(currentTime);
     const [comment, setComment] = useState<string>("");
+    const [selectedContactOption, setSelectedContactOption] = useState("");
 
     const [minimumDate, setMinimumDate] = useState(todayDate);
     const [minimumTime, setMinimumTime] = useState(currentTime);
@@ -37,14 +38,13 @@ const ScheduleACall = () => {
                 setMinimumDate(currentDate);
             }
 
-
-            // else {
-            //     if (currentTime != minimumTime) {
-            //         setMinimumTime(currentTime);
-            //     } else {
-            //         setMinimumTime("");
-            //     }
-            // }
+            else {
+                if (currentTime != minimumTime) {
+                    setMinimumTime(currentTime);
+                } else {
+                    setMinimumTime("");
+                }
+            }
         }, 1000);
     }, [])
 
@@ -71,16 +71,50 @@ const ScheduleACall = () => {
                     <InputField type='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
                     <InputField type='number' placeholder='Phone' value={phone} onChange={e => setPhone(e.target.value)} />
                     <InputField type='number' placeholder='Zip' value={zip} onChange={e => setZip(e.target.value)} />
+
+                    <div className='flex flex-col gap-1'>
+                        <label className='text-[#777]- text-black text-sm'>Add Comment <b className='text-[#fe8f01]'>*</b></label>
+                        <ContactOptionsView currentSelectedOption={selectedContactOption} setOptions={setSelectedContactOption} />
+                    </div>
                     <InputField min={minimumDate} type='date' placeholder='Preferred date' value={preferredDate} onChange={e => setPreferredDate(e.target.value)} />
                     <InputField min={minimumTime} type='time' placeholder='Preferred time' value={preferredTime} onChange={e => setPreferredTime(e.target.value)} />
                     <div className='flex flex-col gap-1'>
                         <label className='text-[#777]- text-black text-sm'>Add Comment <b className='text-[#fe8f01]'>*</b></label>
                         <textarea value={comment} onChange={e => setComment(e.target.value)} placeholder='' className=' px-3 py-2 outline-none border rounded-md focus:border-[#096982]'></textarea>
                     </div>
+
+
+                    <div className='flex justify-center mt-2'>
+                        <button type='submit' className='px-10 py-2 text-white select-none rounded-full bg-[#096982] active:bg-[#0f5a6d]'>Sumit</button>
+                    </div>
                 </form>
             </div>
         </>
     )
+}
+
+
+const ContactOptionsView = ({ currentSelectedOption, setOptions }: { currentSelectedOption: string, setOptions: Function }) => {
+    const OPTIONS = ["Phone", "Email"];
+
+
+
+    const OptionButton = ({ optionText, isSelected }: { optionText: string, isSelected: boolean }) => {
+        return <button className='flex items-center p-3 justify-between border-b rounded active:bg-[#e5e5e5]' onClick={e => setOptions(optionText)}>
+            <span className='text-lg leading-loose'>{optionText}</span>
+            <div className={`min-w-[20px] min-h-[20px] rounded-full border border-[#777] flex items-center justify-center`}>
+                <div className={`w-[50%] aspect-square bg-[#096982] transition-all rounded-full ${isSelected ? 'opacity-100' : 'opacity-0'}`}></div>
+            </div>
+        </button>
+    }
+
+    return <div className='flex flex-col'>
+        {
+            OPTIONS.map((option, index) => {
+                return <OptionButton key={index} optionText={option} isSelected={currentSelectedOption === option} />
+            })
+        }
+    </div>
 }
 
 const getCurrentTime = (addOnHours = 0) => {
