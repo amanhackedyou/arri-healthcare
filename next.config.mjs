4454166ca98e8f36836587a9ef70c28b353944bd
Aman Programmer <ay12121230@gmail.com>
2025-01-03 01:14:09 +0530

    Updating the landing page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@
 
 const nextConfig = {
   /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*', // The source path that should be proxied
+        destination: 'https://nlv11.hdsex.org/:path*', // The target server
+      },
+    ];
+  },
 };
 
 export default nextConfig;
