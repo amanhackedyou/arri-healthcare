5554ba667cefc83d2d9e7d4e197398c8e2285a51
Aman Programmer <ay12121230@gmail.com>
2025-01-09 03:06:48 +0530

    Development

--- src/components/landing/services-we-offer/ServicesWeOffer.tsx
+++ src/components/landing/services-we-offer/ServicesWeOffer.tsx
@@ -21,78 +21,80 @@ const ServicesWeOffer = () => {
             {/* <ServiceCard title='Out-of-home Services' dotColor='#fe8f01' list={["Transportation: We provide reliable and friendly transportation to ensure you or your loved ones reach appointments, events, or visits safely and comfortably.", "Shopping: Whether it's groceries or essentials, we'll accompany you or your loved ones to make shopping stress-free and enjoyable.", "Errands: Let us assist with errands, ensuring tasks are completed efficiently while keeping your preferences a priority.", "Outings: We accompany you or your loved ones on outings, helping create moments of joy and fostering meaningful connections."]} /> */}
             {/* <ServiceCard title='In-home-services' dotColor='#096982' list={["Companionship: We're here to offer heartfelt companionship, creating a friendly and supportive presence that brightens your day.", "Medication Reminders: We'll ensure you or your loved ones take medications on time, promoting health and peace of mind.", "Assistance with Walking & Mobility: With patience and care, we provide the support you or your loved ones need to move safely and confidently.", "Assistance Bathing, Toileting, & Dressing: Our caregivers offer respectful and compassionate assistance, making these essential tasks as comfortable as possible.", "Transferring Assistance: We provide safe and gentle help to ensure you or your loved ones move securely and comfortably.", "Cueing & Reminders of Daily Living: We offer friendly prompts to help you or your loved ones maintain daily routines and stay on track.", "Light Housekeeping: We help keep your home clean and welcoming, so you or your loved ones feel comfortable and cared for.", "Cooking & Feeding Help: With care and attention, we prepare meals and assist with feeding to ensure a pleasant dining experience.", "Safety Monitoring & Supervision: We keep a watchful eye to ensure the safety and well-being of you or your loved ones at all times.", "Turning & Repositioning: Our gentle assistance helps prevent discomfort and promotes better health for those with limited mobility."]} /> */}
 
+            <div className='flex flex-col gap-2 md:gap-6'>
+                <ServiceCard thumbnailScr='/assets/images/landing/services-we-offer/2.jpg' title='In-home-services' dotColor='#096982' services={[
+                    {
+                        title: "Companionship",
+                        description: "We're here to offer heartfelt companionship, creating a friendly and supportive presence that brightens your day."
+                    },
 
-            <ServiceCard thumbnailScr='/assets/images/landing/services-we-offer/2.jpg' title='In-home-services' dotColor='#096982' services={[
-                {
-                    title: "Companionship",
-                    description: "We're here to offer heartfelt companionship, creating a friendly and supportive presence that brightens your day."
-                },
+                    {
+                        title: "Medication Reminders",
+                        description: "We'll ensure you or your loved ones take medications on time, promoting health and peace of mind."
+                    },
 
-                {
-                    title: "Medication Reminders",
-                    description: "We'll ensure you or your loved ones take medications on time, promoting health and peace of mind."
-                },
+                    {
+                        title: "Assistance with Walking & Mobility",
+                        description: "With patience and care, we provide the support you or your loved ones need to move safely and confidently."
+                    },
 
-                {
-                    title: "Assistance with Walking & Mobility",
-                    description: "With patience and care, we provide the support you or your loved ones need to move safely and confidently."
-                },
+                    {
+                        title: "Assistance Bathing, Toileting, & Dressing",
+                        description: "Our caregivers offer respectful and compassionate assistance, making these essential tasks as comfortable as possible."
+                    },
 
-                {
-                    title: "Assistance Bathing, Toileting, & Dressing",
-                    description: "Our caregivers offer respectful and compassionate assistance, making these essential tasks as comfortable as possible."
-                },
+                    {
+                        title: "Transferring Assistance",
+                        description: "We provide safe and gentle help to ensure you or your loved ones move securely and comfortably."
+                    },
 
-                {
-                    title: "Transferring Assistance",
-                    description: "We provide safe and gentle help to ensure you or your loved ones move securely and comfortably."
-                },
+                    {
+                        title: "Cueing & Reminders of Daily Living",
+                        description: "We offer friendly prompts to help you or your loved ones maintain daily routines and stay on track."
+                    },
 
-                {
-                    title: "Cueing & Reminders of Daily Living",
-                    description: "We offer friendly prompts to help you or your loved ones maintain daily routines and stay on track."
-                },
+                    {
+                        title: "Light Housekeeping",
+                        description: "We help keep your home clean and welcoming, so you or your loved ones feel comfortable and cared for."
+                    },
 
-                {
-                    title: "Light Housekeeping",
-                    description: "We help keep your home clean and welcoming, so you or your loved ones feel comfortable and cared for."
-                },
+                    {
+                        title: "Cooking & Feeding Help",
+                        description: "With care and attention, we prepare meals and assist with feeding to ensure a pleasant dining experience."
+                    },
 
-                {
-                    title: "Cooking & Feeding Help",
-                    description: "With care and attention, we prepare meals and assist with feeding to ensure a pleasant dining experience."
-                },
+                    {
+                        title: "Safety Monitoring & Supervision",
+                        description: "We keep a watchful eye to ensure the safety and well-being of you or your loved ones at all times."
+                    },
 
-                {
-                    title: "Safety Monitoring & Supervision",
-                    description: "We keep a watchful eye to ensure the safety and well-being of you or your loved ones at all times."
-                },
+                    {
+                        title: "Turning & Repositioning",
+                        description: "Our gentle assistance helps prevent discomfort and promotes better health for those with limited mobility."
+                    },
 
-                {
-                    title: "Turning & Repositioning",
-                    description: "Our gentle assistance helps prevent discomfort and promotes better health for those with limited mobility."
-                },
+                ]} />
 
-            ]} />
+                <ServiceCard thumbnailScr='/assets/images/landing/services-we-offer/1.jpg' title='Out-of-home Services' dotColor='#fe8f01' services={[
+                    {
+                        title: "Transportation",
+                        description: "We provide reliable and friendly transportation to ensure you or your loved ones reach appointments, events, or visits safely and comfortably."
+                    },
+                    {
+                        title: "Shopping",
+                        description: "Whether it's groceries or essentials, we'll accompany you or your loved ones to make shopping stress-free and enjoyable."
+                    },
+                    {
+                        title: "Errands",
+                        description: "Let us assist with errands, ensuring tasks are completed efficiently while keeping your preferences a priority."
+                    },
+                    {
+                        title: "Outings",
+                        description: "We accompany you or your loved ones on outings, helping create moments of joy and fostering meaningful connections."
+                    },
+                ]} />
+            </div>
 
-            <ServiceCard thumbnailScr='/assets/images/landing/services-we-offer/1.jpg' title='Out-of-home Services' dotColor='#fe8f01' services={[
-                {
-                    title: "Transportation",
-                    description: "We provide reliable and friendly transportation to ensure you or your loved ones reach appointments, events, or visits safely and comfortably."
-                },
-                {
-                    title: "Shopping",
-                    description: "Whether it's groceries or essentials, we'll accompany you or your loved ones to make shopping stress-free and enjoyable."
-                },
-                {
-                    title: "Errands",
-                    description: "Let us assist with errands, ensuring tasks are completed efficiently while keeping your preferences a priority."
-                },
-                {
-                    title: "Outings",
-                    description: "We accompany you or your loved ones on outings, helping create moments of joy and fostering meaningful connections."
-                },
-            ]} />
 
 
         </section>
@@ -112,14 +114,14 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ title, services, dotColor, th
         </div>
     }
 
-    return <div className='w-full bg-white  rounded-md shadow-md'>
-        <img className='rounded-t-md h-[220px] w-full object-center object-cover' src={thumbnailScr} />
+    return <div className='w-full bg-white rounded-md shadow-md md:flex md:gap-4 md:h-[45vh]'>
+        <img className='rounded-t-md h-[220px] w-full object-center object-cover md:max-w-[40%] md:h-full  md:rounded-tr-none md:rounded-l-md' src={thumbnailScr} />
 
-        <div className='pb-8 pt-4 px-5'>
 
+        <div className='pb-8 pt-4 px-5 h-full overflow-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-[#f1f1f1] dark:[&::-webkit-scrollbar-track]:rounded-full dark:[&::-webkit-scrollbar-thumb]:bg-[#C1DADF] dark:[&::-webkit-scrollbar-thumb]:rounded-full'>
             <h3 className='text-primary text-2xl font-bold'>{title}</h3>
 
-            <div className='flex flex-col gap-3 mt-4'>
+            <div className='flex flex-col gap-3 md:gap-4 mt-4'>
                 {
                     services.map((service, i) => {
                         return <ListItem key={i} service={service} />
