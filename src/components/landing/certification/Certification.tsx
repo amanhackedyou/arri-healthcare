afc498218516b64221fbf4f891c52847b7ed1083
Aman Programmer <ay12121230@gmail.com>
2025-01-13 04:56:27 +0530

    Development

--- src/components/landing/certification/Certification.tsx
+++ src/components/landing/certification/Certification.tsx
@@ -9,6 +9,7 @@ import Heading from "@/components/GLOBAL/Heading";
 const Certification = () => {
   return (
     <section className={`${styles.body}`}>
+      <div className='bg-primaryBg h-12 w-full top-0 left-0 rounded-b-[100%]'></div>
       {/* <div className="lg:pt-20- lg:flex lg:flex-col lg:justify-around lg:h-full ">
         <Heading text="Who We Are" />
 
@@ -31,7 +32,9 @@ const Certification = () => {
 
       <div className="w-full flex flex-col items-center relative">
         <div className="flex flex-col relative w-full lg:w-[45%]- md:w-[35rem] px-4- ">
-          <p className='text-[0.9rem] z-20 z-10 text-center absolute px-8 md:px-0 md:w-[150%] top-[20%] md:top-[53%] md:left-[43%] md:translate-x-[-50%] md:translate-y-[-50%] md:text-3xl lg:mx-12 bg-[#00000071]- text-balance font-medium md:leading-[3rem]'>
+          <p style={{
+            filter: 'drop-shadow(0 2px 2px #096a8273)'
+          }} className='text-[0.9rem] z-20 z-10- bg-[#096a8273]- text-center absolute px-8 md:px-0 md:w-[150%] top-[20%] md:top-[53%] md:left-[43%] md:translate-x-[-50%] md:translate-y-[-50%] md:text-3xl lg:mx-12 bg-[#00000071]- text-balance font-medium md:leading-[3rem]'>
             <b className='font-bold'>At Arri Healthcare, We Serve with Compassion</b><br />We provide dedicated support to individuals with developmental disabilities, seniors, and those with unique health needs. As a licensed non-medical home health company, we ensure every client is treated with dignity, respect, and tailored care. Our mission is to create a nurturing environment where everyone feels valued, loved, and empowered to thrive. At Arri Healthcare, your well-being is our purpose.
           </p>
           <img className="z-10" src={Images.ohioMap} alt="Ohio Map" />
@@ -39,7 +42,7 @@ const Certification = () => {
 
         </div>
 
-        <img className="absolute bottom-0 z-0 hidden md:block" src="/assets/icons/wave3.svg" alt="" />
+        {/* <img className="absolute bottom-0 z-0 hidden md:block" src="/assets/icons/wave3.svg" alt="" /> */}
       </div>
 
 
