054d049353470607fb80f5d795cf4e942f339c33
Aman Programmer <ay12121230@gmail.com>
2025-03-21 01:21:57 +0530

    Fixing email sending error

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -2,12 +2,15 @@ export const SECRETS = {
     mail: {
         service: "gmail",
         auth: {
-            user: "kvibeindustries@gmail.com",
-            pass: "lwzm tgoh ghrl fxos",
+            // user: "kvibeindustries@gmail.com",
+            // pass: "lwzm tgoh ghrl fxos",
+            user: "arrisystem@gmail.com",
+            pass: "dlaj jqfr xihb yktp",
         },
     }
 }
 
 export const ADMIN_INFO = {
-    email: "imobcodeofficial@gmail.com"
+    // email: "imobcodeofficial@gmail.com"
+    email: "arrihealthcareservices@gmail.com"
 }
